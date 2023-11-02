2f29656a9ea034042b2c6103a897935ca7b4904d
alamin5g <alaminrakib4864@gmail.com>
2023-11-02 23:55:01 +0600

    Exercise 1.1 from chapter 1.

---
+++ src/Chapter_1_Exercise/DisplayThreeMessage.java
@@ -0,0 +1,9 @@
+package Chapter_1_Exercise;
+
+public class DisplayThreeMessage {
+    public static void main(String[] args){
+        System.out.println("Welcome to Java.");
+        System.out.println("Welcome to Computer Science,");
+        System.out.println("Programming is Fun!");
+    }
+}
