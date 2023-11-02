b2898cf2c7009febb6d39a3a2f99ad9151bc19b9
alamin5g <21303134@iubat.edu>
2023-11-02 22:43:59 +0600

    Three lines statements of my java learning life.

---
+++ src/WelcomeWithThreeMessages.java
@@ -0,0 +1,9 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class WelcomeWithThreeMessages {
+    public static void main(String[] args){
+        System.out.println("Programming is not just an obstacle, it's fun to me!");
+        System.out.println("I love programming and it's my another new start since 2017");
+        System.out.println("Still I could not learn java properly, I mean, I could not use it in a large or production scale");
+    }
+}
