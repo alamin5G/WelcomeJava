dca5f898b1b5d7bd6346d1500da507c1ab2d2a92
alamin5g <alaminrakib4864@gmail.com>
2023-11-03 13:07:12 +0600

    Exercise listing 2.1 with Scanner class

--- src/Listing_2/ComputeArea.java
+++ src/Listing_2/ComputeArea.java
@@ -1,11 +1,15 @@
 package Listing_2;
 
+import java.util.Scanner;
+
 public class ComputeArea {
     public static void main(String[] args) {
         double radius;
         double area;
 
-        radius = 20;
+        System.out.print("Insert the radius of the circle: ");
+        Scanner input = new Scanner(System.in);
+        radius = input.nextDouble();
         area = radius * radius * 3.14159;
         System.out.println("The area for the circle of radius " + radius + " is " + area);
     }
