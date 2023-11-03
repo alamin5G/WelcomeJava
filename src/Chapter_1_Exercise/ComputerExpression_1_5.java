1ac75af0d3cc546cffcca6473371d6a2debffe35
alamin5g <alaminrakib4864@gmail.com>
2023-11-03 13:05:04 +0600

    Exercise listing 2.1

---
+++ src/Chapter_1_Exercise/ComputerExpression_1_5.java
@@ -0,0 +1,7 @@
+package Chapter_1_Exercise;
+
+public class ComputerExpression_1_5 {
+    public static void main(String[] args) {
+        System.out.println("The result of ");
+    }
+}
---
+++ src/Chapter_1_Exercise/DisplayFiveMessages.java
@@ -0,0 +1,11 @@
+package Chapter_1_Exercise;
+
+public class DisplayFiveMessages {
+    public static void main(String[] args){
+        System.out.println("Welcom to Java");
+        System.out.println("Welcom to Java");
+        System.out.println("Welcom to Java");
+        System.out.println("Welcom to Java");
+        System.out.println("Welcom to Java");
+    }
+}
