cfd9184e3256db5a00c0d3746872befd7aebeede
alamin5g <21303134@iubat.edu>
2023-11-03 21:24:15 +0600

    Exponential Operation with Math.pow

---
+++ src/Listing_2/ExponentialOperation.java
@@ -0,0 +1,10 @@
+package Listing_2;
+
+public class ExponentialOperation {
+    public static void main(String[] args){
+        System.out.println("2^3 = " + Math.pow(2, 3));
+        System.out.println("4^0.5 = " + Math.pow(4, 0.5));
+        System.out.println("2^2.5 = " + Math.pow(2, 2.5));
+        System.out.println("2^-2 = " + Math.pow(2, -2));
+    }
+}
