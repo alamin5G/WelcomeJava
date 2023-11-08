0fc5a6592eec86dbb9f7cd5d0ab9b2c75b34edae
alamin5g <21303134@iubat.edu>
2023-11-08 23:00:22 +0600

    do while loop test

---
+++ src/Listing_5/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package Listing_5;
+
+public class DoWhileLoop {
+    public static void main(String[] args){
+
+        int i = 0;
+        do {
+            System.out.println("i = " + i);
+            i++;
+        }while (i<10);
+    }
+}
