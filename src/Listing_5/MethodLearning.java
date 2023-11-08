ad9d9185dd44cf6775764a7752f21dc9a164700a
alamin5g <21303134@iubat.edu>
2023-11-08 23:25:26 +0600

    do while loop test

---
+++ src/Listing_5/MethodLearning.java
@@ -0,0 +1,22 @@
+package Listing_5;
+
+import java.util.Scanner;
+
+public class MethodLearning {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number that will count sum from 1 to your number: ");
+        int times = input.nextInt();
+
+        int sum = sumOfNumber(times);
+        System.out.println("The sum of 1 to " + times + " = " + sum);
+    }
+
+    private static int sumOfNumber(int times) {
+         int value = 0;
+        for ( int i = 1; i <= times; i++) {
+            value = value + i;
+        }
+        return value;
+    }
+}
