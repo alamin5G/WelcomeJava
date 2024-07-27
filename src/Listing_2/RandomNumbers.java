ca7edf549099e0f317f9fe70d4c7885e15e993c2
alamin5g <alaminrakib4864@gmail.com>
2024-07-27 07:35:12 +0600

    random number generate

---
+++ src/Listing_2/RandomNumbers.java
@@ -0,0 +1,40 @@
+package Listing_2;
+
+public class RandomNumbers {
+    public static void main(String[] args) {
+        System.out.println("Random Numbers Example: between 1 to 100");
+        int randomNumber1 = (int) (Math.random() * 100);
+        int randomNumber2 = (int) (Math.random() * 100);
+        System.out.println("Random Number1: " + randomNumber1);
+        System.out.println("Random Number2: " + randomNumber2);
+
+        System.out.println("Random Numbers Example: between 10 to 99");
+        int randomNumber3 = 10 + (int) (Math.random() * 90);
+        System.out.println("Random Number3: " + randomNumber3);
+        System.out.println("Random Numbers Example: between 10 to 99");
+        int randomNumber4 = 10 + (int) (Math.random() * 90);
+        System.out.println("Random Number4: " + randomNumber4);
+
+        System.out.println("Random Numbers Example: between 90 to 99");
+        int randomNumber5 = 90 + (int) (Math.random() * 10);
+        System.out.println("Random Number3: " + randomNumber5);
+        System.out.println("Random Numbers Example: between 10 to 99");
+        int randomNumber6 = 90 + (int) (Math.random() * 10);
+        System.out.println("Random Number4: " + randomNumber6);
+        System.out.println("Random Numbers Example: between 90 to 99");
+        int randomNumber7 = 90 + (int) (Math.random() * 10);
+        System.out.println("Random Number5: " + randomNumber7);
+        System.out.println("Random Numbers Example: between 90 to 99");
+        int randomNumber8 = 90 + (int) (Math.random() * 10);
+        System.out.println("Random Number6: " + randomNumber8);
+        System.out.println("Random Numbers Example: between 90 to 99");
+        int randomNumber9 = 90 + (int) (Math.random() * 10);
+        System.out.println("Random Number7: " + randomNumber9);
+        System.out.println("Random Numbers Example: between 90 to 99");
+        int randomNumber10 = 90 + (int) (Math.random() * 10);
+        System.out.println("Random Number8: " + randomNumber10);
+
+
+
+    }
+}
