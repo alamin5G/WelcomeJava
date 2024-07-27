02c18c37269a1137fd1da948489c7e26b4b4aa90
alamin5g <alaminrakib4864@gmail.com>
2024-07-27 06:00:55 +0600

    Display Binary Octal and Hexa

---
+++ src/Listing_2/DisplayBinaryOctalHex.java
@@ -0,0 +1,9 @@
+package Listing_2;
+
+public class DisplayBinaryOctalHex {
+    public static void main(String[] args) {
+        System.out.println(0B111); //OUTPUT - 7 -- 0B TO INDICATE BINARY
+        System.out.println(07); //OUTPUT - 7 -- 0 TO INDICATE OCTAL
+        System.out.println(0xF); //OUTPUT - 15; -- 0X TO INDICATE HEXADECIMAL
+    }
+}
