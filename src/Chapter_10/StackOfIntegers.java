4426a5bc9e160540a296b3a63a936de975dc3a9a
alamin5g <alaminrakib4864@gmail.com>
2024-08-02 06:36:10 +0600

    Stack with dynamic array size

--- src/Chapter_10/StackOfIntegers.java
+++ src/Chapter_10/StackOfIntegers.java
@@ -1,5 +1,6 @@
 package Chapter_10;
 
+import java.util.Arrays;
 import java.util.EmptyStackException;
 
 public class StackOfIntegers {
@@ -7,7 +8,7 @@ public class StackOfIntegers {
     private int size;
 
     StackOfIntegers(){
-        elementSize = new int[16];
+        elementSize = new int[10];
     }
 
     public StackOfIntegers(int capacity) {
@@ -22,8 +23,12 @@ public class StackOfIntegers {
         return size;
     }
     public void push(int element){
-        elementSize[size] = element;
-        size++;
+       if(size == elementSize.length){
+           int[] newArray = new int[elementSize.length * 2];
+           System.arraycopy(elementSize, 0, newArray, 0, elementSize.length);
+           elementSize = newArray;
+       }
+        elementSize[size++] = element;
     }
     public int pop(){
         if(size == 0){
@@ -56,8 +61,18 @@ class StackImpl {
         st.push(10);
         st.push(20);
         st.push(30);
+        st.push(40);
+        st.push(50);
+        st.push(60);
+        st.push(70);
+        st.push(80);
+        st.push(90);
+        st.push(100);
+        st.push(21);
+        st.push(22);
         System.out.println("Printing stack elements");
         st.printStack();
+        System.out.println("Stack Size : " + st.getSize());
         System.out.println("\nPeeking element : " + st.peek() );
         System.out.println("\nPopped element : " + st.pop());
 
