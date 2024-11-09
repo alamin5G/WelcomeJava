06ab2b9f386b18c6d50441cab36c29ef33a1a97b
Md Al Amin <21303134@iubat.edu>
2024-11-09 15:32:49 +0600

    icpc preli-2024 Problem C

--- src/icpc_preli/Problem3.java
+++ src/icpc_preli/Problem3.java
@@ -1,4 +1,22 @@
 package icpc_preli;
 
+import java.util.Scanner;
+
 public class Problem3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int a = 1; a <= t; a++) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int r = 0;
+            for (int i = 1; i <= n; i++) {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                int z = k / x;
+                r += z;
+            }
+            System.out.println("Case " + a + ": " + r);
+        }
+    }
 }
