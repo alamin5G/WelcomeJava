594e4d9c6c62f3da3a94435051dc6dd495bae745
Md Al Amin <21303134@iubat.edu>
2024-12-11 21:59:35 +0600

    exception handling

--- src/exceptionHandling/Main.java
+++ src/exceptionHandling/Main.java
@@ -1,4 +1,39 @@
 package exceptionHandling;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        try {
+            int result = divide(10, 0);
+
+            int a = 10;
+            int b = 0;
+            int c = a / b;
+            System.out.println(c);
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmetic Exception: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+        } finally {
+            System.out.println("Finally block is always executed");
+        }
+
+    }
+
+    public static int divide(int a, int b) throws ArithmeticException, ArrayIndexOutOfBoundsException {
+
+        if (b == 0) {
+            throw new ArithmeticException("Divide by zero is not allowed");
+        }else {
+            return a / b;
+        }
+
+    }
+
 }
+
+
+
